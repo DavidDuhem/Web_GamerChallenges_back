6b447e196f371f931613d0a719b51bd839474138
David <synlike@gmail.com>
2025-10-18 19:14:17 +0200

    fix: trying waiting for db to be ready for github actions tests

--- src/test/config/global-setup.ts
+++ src/test/config/global-setup.ts
@@ -7,13 +7,34 @@ import { prisma } from "../../../prisma/index.js"
 
 let server: Server
 
+async function waitForPostgres(
+  host = "localhost",
+  port = 55432,
+  user = "test"
+) {
+  const { exec } = await import("child_process")
+  const wait = () => new Promise((resolve) => setTimeout(resolve, 2000))
+
+  for (let i = 0; i < 15; i++) {
+    try {
+      await $`pg_isready -h ${host} -p ${port} -U ${user}`
+      return
+    } catch {
+      console.log("Waiting for Postgres...")
+      await wait()
+    }
+  }
+  throw new Error("Postgres did not become ready in time")
+}
+
 before(async () => {
   const composeFileAbsolutePath = path
     .resolve(import.meta.dirname, "compose.test.yml")
     .replace(/\\/g, "/")
 
   await $`docker compose -f "${composeFileAbsolutePath}" -p gamerchallengestest up -d`
-  await new Promise((r) => setTimeout(r, 1000))
+  //   await new Promise((r) => setTimeout(r, 1000))
+  await waitForPostgres()
 
   const prismaSchemaAbsolutePath = path.resolve(
     import.meta.dirname,
