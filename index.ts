85af801d1dfc56dcd75d4d57b564b9cf1d1d913f
David <synlike@gmail.com>
2026-03-20 11:33:04 +0100

    Setting up vps deployment

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import { app } from "./src/app.js"
 import { logger } from "./src/lib/log.js"
 
 const port = config.server.port
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   logger.info(`🚀 Server started at http://localhost:${port}`)
 })
--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,10 @@ import { xssSanitizer } from "./middlewares/xssSanitizerMiddleware.js"
 import { config } from "../config.js"
 import { router } from "./router.js"
 import { loggerMiddleware } from "./middlewares/loggerMiddleware.js"
-import { limiter } from "./middlewares/rateLimitMiddleware.js"
 
 export const app = express()
 // app.use(limiter) INFO : Source de bugs pour la presentation
+console.log("DEBUG CORS ORIGIN:", config.server.allowedOrigins)
 app.use(cors({ origin: config.server.allowedOrigins, credentials: true }))
 app.use(cookieParser())
 app.disable("x-powered-by")
